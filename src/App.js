a8f28c066cc09306368dc68353122cc45f312e2a
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-04-24 22:26:02 -0400

    App.js imports done, not much else...

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ Not all files need code added.
 Look at each file to see where you need to pass props or add code 
 */
 import React from "react";
-//import ReactDOM from "react-dom";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 import PostsPage from "./components/PostsContainer/PostsPage.js";
@@ -15,12 +14,10 @@ const App = () => {
 
   return (
     <div className="App">
-      {PostsPage/* Add imported components here to render them */}
+      <PostsPage/>
+      <SearchBar/>
     </div>
   );
 };
-
-//const rootElement = document.getElementById('root');
-//ReactDOM.render(<App {...PostsPage}/>, rootElement); 
 
 export default App;
